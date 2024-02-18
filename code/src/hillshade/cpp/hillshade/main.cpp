6e526fcc5821649b338482d914d8cea30189a8d9
nathanstouffer <nathanstouffer1999@gmail.com>
2024-02-18 14:44:13 -0700

    Add stf dependency (#2)
    * add stf as a FetchContent dependency
    * remove submodule clone in CI config
    * add root level CMakeLists.txt

--- code/src/hillshade/cpp/hillshade/main.cpp
+++ code/src/hillshade/cpp/hillshade/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#include <stf/stf.hpp>
+
 int main()
 {
     std::cout << "hello world" << std::endl;
+    std::cout << "vec3: " << stff::vec3(5) << std::endl;
     return 0;
 }
