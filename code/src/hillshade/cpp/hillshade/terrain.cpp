ad97a47b1fe8568fee48db5ed8ba01ef239c614e
Nathan Stouffer <nathanstouffer1999@gmail.com>
2025-01-18 11:38:54 -0700

    compute spatial bounds of tiff

--- code/src/hillshade/cpp/hillshade/terrain.cpp
+++ code/src/hillshade/cpp/hillshade/terrain.cpp
@@ -15,6 +15,11 @@ namespace hillshade
             return;
         }
 
+        auto tmp0 = dataset->GetProjectionRef();
+        auto tmp1 = dataset->GetSpatialRef();
+        std::cout << tmp0 << tmp1 << std::endl;
+        dataset->GetGeoTransform(m_geo_transform.data());
+
         // grab dimensions
         int width = dataset->GetRasterXSize();
         int height = dataset->GetRasterYSize();
@@ -27,6 +32,18 @@ namespace hillshade
 
         m_width = static_cast<size_t>(width);
         m_height = static_cast<size_t>(height);
+
+        // compute spatial bounds
+        {
+            m_top_left = stfd::vec2(m_geo_transform[0], m_geo_transform[3]);
+            m_bottom_right = m_top_left;
+            m_bottom_right += static_cast<double>(width) * stfd::vec2(m_geo_transform[1], m_geo_transform[4]);
+            m_bottom_right += static_cast<double>(height) * stfd::vec2(m_geo_transform[2], m_geo_transform[5]);
+
+            stfd::vec2 center = 0.5 * (m_top_left + m_bottom_right);
+            m_top_left -= center;
+            m_bottom_right -= center;
+        }
 
         GDALClose(dataset);
     }
