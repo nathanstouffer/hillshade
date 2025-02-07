fa88a3204ff710e44343bb9699456f3726fb2056
Nathan Stouffer <nathanstouffer1999@gmail.com>
2025-02-06 17:30:34 -0700

    get working version of docker image

--- converter/convert.py
+++ converter/convert.py
@@ -1,3 +1,18 @@
 import constants
+import os
+from osgeo import gdal
 
-print(f"{constants.DATA_DIR}")
+def convert(filename: str):
+    name = filename.split('.')[0]
+    print(name)
+    dataset = gdal.Open(f"{constants.DATA_DIR}/tiffs/{filename}", gdal.GA_ReadOnly)
+    band = dataset.GetRasterBand(1)
+    arr = band.ReadAsArray()
+    print(arr.shape)
+    dataset.Close()
+    # with rasterio.open(f"{constants.DATA_DIR}/tiffs/{filename}") as tif:
+    #     data = tif.read()
+
+gdal.UseExceptions()
+for filename in os.listdir(f"{constants.DATA_DIR}/tiffs/"):
+    convert(filename)
