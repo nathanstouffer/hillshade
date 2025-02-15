fafaffac0266632e89e9a6e867695a3b9ce494b3
Nathan Stouffer <nathanstouffer1999@gmail.com>
2025-02-15 12:59:41 -0700

    remove extraneous code

--- transform/cut.py
+++ transform/cut.py
@@ -39,38 +39,6 @@ def cut(args: argparse.Namespace):
     trg.Close()
 
     print(f"----------{"-" * len(f" processing {name}")}-----------")
-    return
-
-    # read tif
-    print("reading tif")
-    dataset = gdal.Open(f"{constants.DATA_DIR}//{filename}", gdal.GA_ReadOnly)
-    geotransform = dataset.GetGeoTransform()
-    band = dataset.GetRasterBand(1)
-    values = band.ReadAsArray()
-    dataset.Close()
-
-    # compute spatial information
-    print("computing metadata")
-    width, height = values.shape
-    top_left = [geotransform[0], geotransform[3]]
-    bottom_right = [
-        geotransform[0] + width * geotransform[1] + height * geotransform[2],
-        geotransform[3] + width * geotransform[4] + height * geotransform[5]
-    ]
-    metadata = {
-        "min": [top_left[0], bottom_right[1]],
-        "max": [bottom_right[0], top_left[1]]
-    }
-    with open(f"{constants.DATA_DIR}/terrarium/{name}.json", "w") as f:
-        json.dump(metadata, f, indent=4)
-
-    # encode image
-    print("encoding to terrarium")
-    terrarium = encode_terrarium_array(values)
-
-    print("saving encoded image")
-    img = Image.fromarray(terrarium)
-    img.save(f"{constants.DATA_DIR}/terrarium/{name}.png")
 
 gdal.UseExceptions()
 
