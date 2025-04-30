000c9892ef0b3795100f77160798a6b8a58b673c
Nathan Stouffer <nathanstouffer1999@gmail.com>
2025-04-29 22:24:07 -0600

    tweak configuration

--- code/src/hillshader/include/private/hillshader/camera/config.hpp
+++ code/src/hillshader/include/private/hillshader/camera/config.hpp
@@ -5,7 +5,7 @@ namespace hillshader::camera
 
     struct config
     {
-        static constexpr float c_min_terrain_offset = 0.5;
+        static constexpr float c_min_terrain_offset = 1.f;
     };
 
 }
